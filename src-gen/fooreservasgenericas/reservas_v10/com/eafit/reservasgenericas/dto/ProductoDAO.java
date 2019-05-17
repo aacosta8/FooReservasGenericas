3a55322cf3b03d123544fd52779dd1c05fe341ea
aacosta8 <aacosta8@eafit.edu.co>
2019-05-16 22:26:47 -0500

    First commit - Se agrega la linea base de producto.

---
+++ src-gen/fooreservasgenericas/reservas_v10/com/eafit/reservasgenericas/dto/ProductoDAO.java
@@ -0,0 +1,41 @@
+package fooreservasgenericas.reservas_v10.com.eafit.reservasgenericas.dto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import
+fooreservasgenericas.reservas_v10.com.eafit.reservasgenericas.bd.Conexion;
+/*** added by dProductoDAO* modified by dAgregarProducto
+ */
+public class ProductoDAO {
+	Statement st = null;
+	PreparedStatement ps = null;
+	ResultSet rs = null;
+	Connection conn = null;
+	Conexion conexion = null;
+	/*** added by dAgregarProducto
+	 */
+	public void agregar(Producto producto) {
+		try {
+			conn = Conexion.getConexion();
+			String query = "INSERT INTO categoria (nombre, descripcion) values (?,?)";
+			ps = conn.prepareStatement(query);
+			ps.setString(1, producto.getNombre());
+			ps.setString(2, producto.getDescripcion());
+			ps.executeUpdate();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				conn.close();
+			}
+			catch(Exception e) {
+			}
+		}
+	}
+}
---
+++ src-gen/fooreservasgenericas/reservas_v10/com/eafit/reservasgenericas/fop/AdminProductos.java
@@ -0,0 +1,22 @@
+package fooreservasgenericas.reservas_v10.com.eafit.reservasgenericas.fop;
+
+import java.util.ArrayList;
+import java.util.List;
+import
+fooreservasgenericas.reservas_v10.com.eafit.reservasgenericas.dto.Producto;
+import
+fooreservasgenericas.reservas_v10.com.eafit.reservasgenericas.dto.ProductoDAO;
+/*** added by dAdminProductos* modified by dAgregarProducto
+ */
+public class AdminProductos {
+	ProductoDAO productoDAO = new ProductoDAO();
+	Producto producto = new Producto();
+	/*** added by dAgregarProducto
+	 */
+	public void agregar(String nombre, String descripcion) throws Exception {
+		Producto producto = new Producto();
+		producto.setNombre(nombre);
+		producto.setDescripcion(descripcion);
+		productoDAO.agregar(producto);
+	}
+}
