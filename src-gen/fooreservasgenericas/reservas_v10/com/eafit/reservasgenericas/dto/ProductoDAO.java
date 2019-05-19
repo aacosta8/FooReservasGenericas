bb4eb7099847f7eaad0af797bb0cd9550e128a68
aacosta8 <aacosta8@eafit.edu.co>
2019-05-18 19:07:11 -0500

    Eliminar y listar productos

--- src-gen/fooreservasgenericas/reservas_v10/com/eafit/reservasgenericas/dto/ProductoDAO.java
+++ src-gen/fooreservasgenericas/reservas_v10/com/eafit/reservasgenericas/dto/ProductoDAO.java
@@ -4,11 +4,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import
 fooreservasgenericas.reservas_v10.com.eafit.reservasgenericas.bd.Conexion;
 import
 fooreservasgenericas.reservas_v10.com.eafit.reservasenericas.modelos.Producto;
-/*** added by dProductoDAO* modified by dAgregarProducto
+/*** added by dProductoDAO* modified by dAgregarProducto* modified by
+dModificarProducto* modified by dEliminarProducto* modified by
+dListarProductos
  */
 public class ProductoDAO {
 	Statement st = null;
@@ -41,5 +45,85 @@ public class ProductoDAO {
 			catch(Exception e) {
 			}
 		}
+	}
+	/*** added by dModificarProducto
+	 */
+	public void modificar(Producto producto) {
+		try {
+			conn = Conexion.getConexion();
+			String query =
+			"UPDATE producto set nombre=?, descripcion=?, precio=?, cantidadDisponible=?, tipo=?,  WHERE idProducto=?";
+			ps = conn.prepareStatement(query);
+			ps.setString(1, producto.getNombre());
+			ps.setString(2, producto.getDescripcion());
+			ps.setFloat(3, producto.getPrecio());
+			ps.setInt(4, producto.getCantidadDisponible());
+			ps.setString(5, producto.getTipo());
+			ps.setInt(6, producto.getIdProducto());
+			ps.executeUpdate();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				conn.close();
+			}
+			catch(Exception e) {
+			}
+		}
+	}
+	/*** added by dEliminarProducto
+	 */
+	public void eliminar(Producto producto) {
+		try {
+			conn = Conexion.getConexion();
+			String query = "DELETE FROM producto WHERE idProducto=?";
+			ps = conn.prepareStatement(query);
+			ps.setInt(1, producto.getIdProducto());
+			ps.executeUpdate();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				conn.close();
+			}
+			catch(Exception e) {
+			}
+		}
+	}
+	/*** added by dListarProductos
+	 */
+	public List<Producto> listar() {
+		List<Producto> resultado = new ArrayList<Producto>();
+		try {
+			conn = Conexion.getConexion();
+			String query = "SELECT id, nombre, descripcion FROM categoria";
+			ps = conn.prepareStatement(query);
+			ResultSet registros = ps.executeQuery();
+			while(registros.next()) {
+				Producto producto = new Producto();
+				producto.setIdProducto(registros.getInt(1));
+				producto.setNombre(registros.getString(2));
+				producto.setDescripcion(registros.getString(3));
+				producto.setPrecio(registros.getFloat(4));
+				producto.setCantidadDisponible(registros.getInt(5));
+				producto.setTipo(registros.getString(6));
+				resultado.add(producto);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				conn.close();
+			}
+			catch(Exception e) {
+			}
+		}
+		return resultado;
 	}
 }
--- src-gen/fooreservasgenericas/reservas_v10/com/eafit/reservasgenericas/fop/AdminProductos.java
+++ src-gen/fooreservasgenericas/reservas_v10/com/eafit/reservasgenericas/fop/AdminProductos.java
@@ -1,10 +1,13 @@
 package fooreservasgenericas.reservas_v10.com.eafit.reservasgenericas.fop;
 
+import java.util.List;
 import
 fooreservasgenericas.reservas_v10.com.eafit.reservasenericas.modelos.Producto;
 import
 fooreservasgenericas.reservas_v10.com.eafit.reservasgenericas.dto.ProductoDAO;
-/*** added by dAdminProductos* modified by dAgregarProducto
+/*** added by dAdminProductos* modified by dAgregarProducto* modified by
+dModificarProducto* modified by dEliminarProducto* modified by
+dListarProductos
  */
 public class AdminProductos {
 	ProductoDAO productoDAO = new ProductoDAO();
@@ -20,5 +23,30 @@ public class AdminProductos {
 		producto.setCantidadDisponible(cantidadDisponible);
 		producto.setTipo(tipo);
 		productoDAO.agregar(producto);
+	}
+	/*** added by dModificarProducto
+	 */
+	public void modificar(int idProducto, String nombre, String descripcion,
+		float precio, int cantidadDisponible, String tipo) throws Exception {
+		Producto producto = new Producto();
+		producto.setIdProducto(idProducto);
+		producto.setNombre(nombre);
+		producto.setDescripcion(descripcion);
+		producto.setPrecio(precio);
+		producto.setCantidadDisponible(cantidadDisponible);
+		producto.setTipo(tipo);
+		productoDAO.modificar(producto);
+	}
+	/*** added by dEliminarProducto
+	 */
+	public void eliminar(Integer idProducto) throws Exception {
+		Producto producto = new Producto();
+		producto.setIdProducto(idProducto);
+		productoDAO.eliminar(producto);
+	}
+	/*** added by dListarProductos
+	 */
+	public List<Producto> listar() throws Exception {
+		return productoDAO.listar();
 	}
 }
