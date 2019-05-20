eb867eb7d81002408149e099c26dfeb20b3037d1
aacosta8 <aacosta8@eafit.edu.co>
2019-05-20 07:05:46 -0500

    Refactor

--- src-gen/fooreservasgenericas/vuelos_v10/com/eafit/reservapeliculas/vistas/ReservaVueloIndexGUI.java
+++ src-gen/fooreservasgenericas/vuelos_v10/com/eafit/reservapeliculas/vistas/ReservaVueloIndexGUI.java
@@ -1,20 +1,15 @@
-package fooreservasgenericas.vuelos_v10.com.eafit.reservasgenericas.vistas;
+package fooreservasgenericas.vuelos_v10.com.eafit.reservapeliculas.vistas;
 
-import javax.swing.*;
-import java.awt.EventQueue;
-import java.awt.event.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-/*** added by dIndexVuelos
+/*** added by dAgregarReservaVueloUI
  */
-public class ReservaPeliculaIndexGUI extends javax.swing.JFrame {
-	public ReservaPeliculaIndexGUI() {
+public class ReservaVueloIndexGUI extends javax.swing.JFrame {
+	public ReservaVueloIndexGUI() {
 		initComponents();
 	}
 	private void initComponents() {
 		registrarUsuarios = new javax.swing.JButton();
-		registrarPeliculas = new javax.swing.JButton();
-		reservarPelicula = new javax.swing.JButton();
+		registrarVuelo = new javax.swing.JButton();
+		reservarVuelo = new javax.swing.JButton();
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 		setTitle("Inicio");
 		registrarUsuarios.setText("Registrar Usuarios");
@@ -23,16 +18,16 @@ public class ReservaPeliculaIndexGUI extends javax.swing.JFrame {
 					registrarUsuariosActionPerformed(evt);
 				}
 			});
-		registrarPeliculas.setText("Agregar Peliculas");
-		registrarPeliculas.addActionListener(new java.awt.event.ActionListener() {
+		registrarVuelo.setText("Agregar Vuelo");
+		registrarVuelo.addActionListener(new java.awt.event.ActionListener() {
 				public void actionPerformed(java.awt.event.ActionEvent evt) {
-					registrarPeliculasActionPerformed(evt);
+					registrarVueloActionPerformed(evt);
 				}
 			});
-		reservarPelicula.setText("Reservar Pelicula");
-		reservarPelicula.addActionListener(new java.awt.event.ActionListener() {
+		reservarVuelo.setText("Reservar Vuelo");
+		reservarVuelo.addActionListener(new java.awt.event.ActionListener() {
 				public void actionPerformed(java.awt.event.ActionEvent evt) {
-					reservarPeliculaActionPerformed(evt);
+					reservarVueloActionPerformed(evt);
 				}
 			});
 		javax.swing.GroupLayout layout = new
@@ -43,30 +38,26 @@ public class ReservaPeliculaIndexGUI extends javax.swing.JFrame {
 					Short.MAX_VALUE).addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING,
 						false).addComponent(registrarUsuarios,
 						javax.swing.GroupLayout.DEFAULT_SIZE, 129,
-						Short.MAX_VALUE).addComponent(registrarPeliculas,
+						Short.MAX_VALUE).addComponent(registrarVuelo,
 						javax.swing.GroupLayout.DEFAULT_SIZE,
 						javax.swing.GroupLayout.DEFAULT_SIZE,
-						Short.MAX_VALUE).addComponent(reservarPelicula,
+						Short.MAX_VALUE).addComponent(reservarVuelo,
 						javax.swing.GroupLayout.DEFAULT_SIZE,
 						javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)).addGap(35, 35,
 					35)));
 		layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout.createSequentialGroup().addGap(35,
 					35, 35).addComponent(registrarUsuarios).addGap(18, 18,
-					18).addComponent(registrarPeliculas).addGap(18, 18,
-					18).addComponent(reservarPelicula).addContainerGap(35,
-					Short.MAX_VALUE)));
+					18).addComponent(registrarVuelo).addGap(18, 18,
+					18).addComponent(reservarVuelo).addContainerGap(35, Short.MAX_VALUE)));
 		pack();
 	}
-	private void registrarPeliculasActionPerformed(java.awt.event.ActionEvent
-		evt) {
-		AgregarPeliculaGUI agregarPeliculaGUI = new AgregarPeliculaGUI();
-		agregarPeliculaGUI.setVisible(true);
+	private void registrarVueloActionPerformed(java.awt.event.ActionEvent evt) {
+		AgregarVueloGUI agregarVueloGUI = new AgregarVueloGUI();
+		agregarVueloGUI.setVisible(true);
 	}
-	private void reservarPeliculaActionPerformed(java.awt.event.ActionEvent evt)
-	{
-		AgregarReservaPeliculaGUI agregarReservaGUI = new
-		AgregarReservaPeliculaGUI();
-		agregarReservaGUI.setVisible(true);
+	private void reservarVueloActionPerformed(java.awt.event.ActionEvent evt) {
+		AgregarReservaVueloGUI agregarVueloGUI = new AgregarReservaVueloGUI();
+		agregarVueloGUI.setVisible(true);
 	}
 	private void registrarUsuariosActionPerformed(java.awt.event.ActionEvent evt)
 	{
@@ -84,28 +75,28 @@ public class ReservaPeliculaIndexGUI extends javax.swing.JFrame {
 			}
 		}
 		catch(ClassNotFoundException ex) {
-			java.util.logging.Logger.getLogger(ReservaPeliculaIndexGUI.class.getName()).log(java.util.logging.Level.SEVERE,
+			java.util.logging.Logger.getLogger(ReservaVueloIndexGUI.class.getName()).log(java.util.logging.Level.SEVERE,
 				null, ex);
 		}
 		catch(InstantiationException ex) {
-			java.util.logging.Logger.getLogger(ReservaPeliculaIndexGUI.class.getName()).log(java.util.logging.Level.SEVERE,
+			java.util.logging.Logger.getLogger(ReservaVueloIndexGUI.class.getName()).log(java.util.logging.Level.SEVERE,
 				null, ex);
 		}
 		catch(IllegalAccessException ex) {
-			java.util.logging.Logger.getLogger(ReservaPeliculaIndexGUI.class.getName()).log(java.util.logging.Level.SEVERE,
+			java.util.logging.Logger.getLogger(ReservaVueloIndexGUI.class.getName()).log(java.util.logging.Level.SEVERE,
 				null, ex);
 		}
 		catch(javax.swing.UnsupportedLookAndFeelException ex) {
-			java.util.logging.Logger.getLogger(ReservaPeliculaIndexGUI.class.getName()).log(java.util.logging.Level.SEVERE,
+			java.util.logging.Logger.getLogger(ReservaVueloIndexGUI.class.getName()).log(java.util.logging.Level.SEVERE,
 				null, ex);
 		}
 		java.awt.EventQueue.invokeLater(new Runnable() {
 				public void run() {
-					new ReservaPeliculaIndexGUI().setVisible(true);
+					new ReservaVueloIndexGUI().setVisible(true);
 				}
 			});
 	}
-	private javax.swing.JButton registrarPeliculas;
 	private javax.swing.JButton registrarUsuarios;
-	private javax.swing.JButton reservarPelicula;
+	private javax.swing.JButton registrarVuelo;
+	private javax.swing.JButton reservarVuelo;
 }
